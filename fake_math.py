205dd86b0ac44512b9fa2510fd912fe10935178f
Aleksandr <alex.kharchenko@mail.ru>
2024-06-06 15:52:28 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first , second):
+    f1 = float(first)
+    s1 = float(second)
+
+    if s1 != 0:
+        return (f1/s1)
+    else:
+        return ('Ошибка')
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+def divide(first , second):
+    f1 = float(first)
+    s1 = float(second)
+
+    if s1 != 0:
+        return (f1/s1)
+    else:
+        return (inf)
+
